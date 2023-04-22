e1a23c609201c7513a2a6f4854e8b76b62b8daf2
wuwei <1999wuwei@gmail.com>
2023-04-22 23:16:57 +0800

    test

--- scripts/iconsRename.js
+++ scripts/iconsRename.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const { join } = require('path')
 const { v4: uuidv4 } = require('uuid')
+const { stringify } = require('qs')
 
 /** 获取图标文件路径 */
 const getIconPath = (iconPath = '') => join(__dirname, '../src/assets/gtaIcons', iconPath)
@@ -8,23 +9,25 @@ const getIconPath = (iconPath = '') => join(__dirname, '../src/assets/gtaIcons',
 /** 重命名icon */
 const iconsRename = () => {
   const icons = []
+  const unnameIcons = []
 
-  fs.readdirSync(getIconPath()).forEach(item => {
-    let iconName = item
-
+  fs.readdirSync(getIconPath()).forEach((item, index) => {
     /** 如果还没生成过id，则需要新生成id */
     if (item.indexOf('id:') !== 0) {
-      iconName = `id:${uuidv4().split('-').join('')}.png`
-      fs.renameSync(getIconPath(item), getIconPath(iconName))
+      const iconName = `${stringify({ id: uuidv4().split('-').join(''), sort: index + 1 })}.png`
+      // fs.renameSync(getIconPath(item), getIconPath(iconName))
+      unnameIcons.push(iconName)
+    } else {
+      icons.push(item)
     }
-
-    icons.push(iconName)
   })
 
-  fs.writeFileSync(
-    join(__dirname, '../src/config/icons.json'),
-    JSON.stringify({ length: icons.length, list: icons }, null, 2)
-  )
+  console.log(11111, unnameIcons)
+
+  // fs.writeFileSync(
+  //   join(__dirname, '../src/config/icons.json'),
+  //   JSON.stringify({ length: icons.length, list: icons }, null, 2)
+  // )
 }
 
 iconsRename()
