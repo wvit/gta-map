6672b8b1cb79e9437ae8e323b5a57f1a0d28be02
wuwei <1999wuwei@gmail.com>
2023-04-22 22:44:05 +0800

    重新生成图标文件名称

--- scripts/iconsRename.js
+++ scripts/iconsRename.js
@@ -5,16 +5,26 @@ const { v4: uuidv4 } = require('uuid')
 /** 获取图标文件路径 */
 const getIconPath = (iconPath = '') => join(__dirname, '../src/assets/gtaIcons', iconPath)
 
-fs.readdirSync(getIconPath()).forEach((item, index) => {
-  const newName = `${index + 1}.png`
-  const iconPath = getIconPath(newName)
+/** 重命名icon */
+const iconsRename = () => {
+  const icons = []
 
-  /** 如果已经存在同名文件，就先重命名 */
-  if (item !== newName && fs.existsSync(iconPath)) {
-    fs.renameSync(iconPath, getIconPath(`${Math.random()}${index}.png`.slice(2)))
-  }
+  fs.readdirSync(getIconPath()).forEach(item => {
+    let iconName = item
 
-  fs.renameSync(getIconPath(item), iconPath)
-})
+    /** 如果还没生成过id，则需要新生成id */
+    if (item.indexOf('id:') !== 0) {
+      iconName = `id:${uuidv4().split('-').join('')}.png`
+      fs.renameSync(getIconPath(item), getIconPath(iconName))
+    }
 
-console.log(11111, uuidv4().split('-').join(''))
+    icons.push(iconName)
+  })
+
+  fs.writeFileSync(
+    join(__dirname, '../src/config/icons.json'),
+    JSON.stringify({ length: icons.length, list: icons }, null, 2)
+  )
+}
+
+iconsRename()
