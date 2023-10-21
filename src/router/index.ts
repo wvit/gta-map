64f2a7a3dbbcb4274ff96b3142b7c86ff74fd09c
wuwei <1999wuwei@gmail.com>
2023-10-21 15:00:14 +0800

    调整配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'mapHome',
-      component: () => import('../views/MapHome/index.vue'),
+      component: () => import('../views/Home/index.vue'),
     },
   ],
 })
