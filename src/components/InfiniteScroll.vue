0570f65fc0d353e5ac8199b926d40f7bdba4251a
wuwei <1999wuwei@gmail.com>
2023-11-03 18:14:46 +0800

    修复bug

--- src/components/InfiniteScroll.vue
+++ src/components/InfiniteScroll.vue
@@ -1,6 +1,6 @@
 <template>
   <slot></slot>
-  <div ref="targetInstance" :style="props.triggerStyle">
+  <div ref="triggerInstance" :style="{ marginTop: '-10px', ...props.triggerStyle }">
     <slot name="triggerChildren"></slot>
   </div>
 </template>
@@ -22,24 +22,24 @@ const props = withDefaults(
 )
 
 /** 检查滚动触底的节点实例 */
-const targetInstance = ref()
+const triggerInstance = ref()
 
-/** 判断<targetInstance>节点是否触底的观察器 */
-const targetObserver = ref()
+/** 判断<triggerInstance>节点是否触底的观察器 */
+const triggerObserver = ref()
 
 /** 监听父级列表容器高度变化的观察器 */
-const targetParentObserver = ref()
+const triggerParentObserver = ref()
 
-/** 标记<targetInstance>节点是否从未被隐藏过 */
-const targetVisible = ref(true)
+/** 标记<triggerInstance>节点是否从未被隐藏过 */
+const triggerVisible = ref(true)
 
 /** 检查节点是否已经被隐藏 */
-const checkTargetHide = () => {
-  const { previousElementSibling, parentNode } = targetInstance.value
+const checktriggerHide = () => {
+  const { previousElementSibling, parentNode } = triggerInstance.value
 
   if (previousElementSibling?.offsetHeight > parentNode.offsetHeight + 10) {
-    targetVisible.value = false
-    targetParentObserver.value.disconnect?.()
+    triggerVisible.value = false
+    triggerParentObserver.value.disconnect?.()
   } else {
     /** 如果目标还没被隐藏，就再加载一页 */
     emit('hitBottom')
@@ -48,23 +48,23 @@ const checkTargetHide = () => {
 
 onMounted(() => {
   /** 仅在目标节点存在时，绑定监听一次 */
-  if (targetObserver.value || !targetInstance.value) return
+  if (triggerObserver.value || !triggerInstance.value) return
 
   /** 监听触底节点是否已经显示 */
-  targetObserver.value = new IntersectionObserver(entries => {
+  triggerObserver.value = new IntersectionObserver(entries => {
     /** 判断目标节点是否已经滚动到可视区，并且目标节点已经被滚动条隐藏过 */
-    if (entries[0].isIntersecting && targetVisible.value === false) {
+    if (entries[0].isIntersecting && triggerVisible.value === false) {
       emit('hitBottom')
     } else {
-      checkTargetHide()
+      checktriggerHide()
     }
   }, props.observerOptions)
 
   /** 监听列表容器高度变化 */
-  targetParentObserver.value = new MutationObserver(checkTargetHide)
+  triggerParentObserver.value = new MutationObserver(checktriggerHide)
 
-  targetObserver.value.observe(targetInstance.value)
-  targetParentObserver.value.observe(targetInstance.value.parentNode, {
+  triggerObserver.value.observe(triggerInstance.value)
+  triggerParentObserver.value.observe(triggerInstance.value.parentNode, {
     /** 子节点的变动（新增、删除或者更改） */
     childList: true,
     /** 属性的变动 */
@@ -77,7 +77,7 @@ onMounted(() => {
 })
 
 onUnmounted(() => {
-  targetObserver.value?.disconnect?.()
-  targetParentObserver.value?.disconnect?.()
+  triggerObserver.value?.disconnect?.()
+  triggerParentObserver.value?.disconnect?.()
 })
 </script>
