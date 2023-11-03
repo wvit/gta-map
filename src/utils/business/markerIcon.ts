f7e643f9072d49ea5be32d88d9a4444ef6a26b57
wuwei <1999wuwei@gmail.com>
2023-11-03 17:54:03 +0800

    调整图标大小

--- src/utils/business/markerIcon.ts
+++ src/utils/business/markerIcon.ts
@@ -35,7 +35,7 @@ export const createMarkerIcon = (options: {
     src: iconSrc,
     draggable: false,
     title: '双击移除此图标',
-  }).setStyle({ width: '30px', height: '30px', cursor: 'pointer' })
+  }).setStyle({ width: '28px', height: '28px', cursor: 'pointer' })
   const customOverlay = new BMapGL.CustomOverlay(() => markerIcon, {
     point,
     map: mapInstance,
